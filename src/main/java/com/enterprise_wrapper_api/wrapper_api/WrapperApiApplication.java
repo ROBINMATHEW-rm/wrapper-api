b6700cc8afe8e426391b16f46cb6ff70d170f594
ROBINMATHEW-rm <robinmathew619619@gmail.com>
2025-11-09 14:17:28 +0530

    Initial commit

---
+++ src/main/java/com/enterprise_wrapper_api/wrapper_api/WrapperApiApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise_wrapper_api.wrapper_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrapperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrapperApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enterprise_wrapper_api/wrapper_api/WrapperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise_wrapper_api.wrapper_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrapperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
