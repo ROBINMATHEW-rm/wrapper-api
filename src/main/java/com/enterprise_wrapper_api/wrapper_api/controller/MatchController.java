f6f869d9bafcc98635b7254c1b877e4b132f5c32
Robin Mathew <robin.mathew619619@gmail.com>
2025-12-14 21:11:30 +0530

    Added new file

--- src/main/java/com/enterprise_wrapper_api/wrapper_api/controller/MatchController.java
+++ src/main/java/com/enterprise_wrapper_api/wrapper_api/controller/MatchController.java
@@ -1,42 +1,39 @@
 package com.enterprise_wrapper_api.wrapper_api.controller;
+
 import com.enterprise_wrapper_api.wrapper_api.model.ResumeMatchRequest;
 import com.enterprise_wrapper_api.wrapper_api.model.ResumeMatchResponse;
 import com.enterprise_wrapper_api.wrapper_api.service.impl.ResumeMatchService;
-import org.apache.tika.Tika;
-import org.apache.tika.exception.TikaException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 @RestController
-@RequestMapping("/api/resume")
 public class MatchController {
 
-    @Autowired
-    private ResumeMatchService matchService;
+    private final ResumeMatchService matchService;
 
-    private final Tika tika = new Tika();
+    public MatchController(ResumeMatchService matchService) {
+        this.matchService = matchService;
+    }
 
+    // ✅ JSON input (resume text + job description)
     @PostMapping("/match")
-    public ResponseEntity<ResumeMatchResponse> match(@RequestBody ResumeMatchRequest request) {
+    public ResponseEntity<ResumeMatchResponse> match(
+            @RequestBody ResumeMatchRequest request) {
+
         ResumeMatchResponse response = matchService.getMatch(request);
         return ResponseEntity.ok(response);
     }
 
+    // ✅ File upload (PDF/DOCX) + job description
     @PostMapping("/upload")
     public ResponseEntity<ResumeMatchResponse> upload(
             @RequestParam("file") MultipartFile file,
-            @RequestParam("jobDescription") String jobDescription) throws IOException, TikaException {
+            @RequestParam("jobDescription") String jobDescription) {
 
-        String resumeText = tika.parseToString(file.getInputStream());
-        ResumeMatchRequest request = new ResumeMatchRequest();
-        request.setResumeText(resumeText);
-        request.setJobDescription(jobDescription);
+        ResumeMatchResponse response =
+                matchService.matchUploadedResume(file, jobDescription);
 
-        ResumeMatchResponse response = matchService.getMatch(request);
         return ResponseEntity.ok(response);
     }
 }
